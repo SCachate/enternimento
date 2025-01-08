8bbd7d78bbce249ea5b31f4e978144930e5e2fe5
Sidnei Cachate <cachate@gmail.com>
2025-01-08 12:36:33 -0300

    buscando dados na API

--- filmes/Controllers/HomeController.cs
+++ filmes/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 using filmes.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System.Diagnostics;
 
 namespace filmes.Controllers
@@ -17,7 +18,42 @@ namespace filmes.Controllers
         [Authorize]
         public IActionResult Index()
         {
-            return View();
+            List<SugestoesModel> model = new List<SugestoesModel>();
+            using (var client = new HttpClient())
+            {                
+                client.BaseAddress = new Uri("https://api.themoviedb.org/");
+                client.DefaultRequestHeaders.Add("Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZjM5ZTk5Y2U1ZWFlYmRjMWY4NzM4ZTE0OWFkNDY4YyIsIm5iZiI6MTczNjM0Mzk2Mi44OTEsInN1YiI6IjY3N2U4MTlhMjE4ZmQ1N2FjZjRlNjZmNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.RrTn4u_NUnuFsJU6foiUWkB9bHoldSbaJBmwimokRsg");
+
+                var response = client.GetAsync("3/discover/movie?include_adult=false&include_video=false&language=pt-BR&page=1&primary_release_year=2025&sort_by=popularity.desc").Result;
+                if (response.IsSuccessStatusCode)
+                {
+
+                    var sugestoes = response.Content.ReadAsStringAsync().Result;
+                    dynamic intermediario = JsonConvert.DeserializeObject(sugestoes)!;
+
+                    if (intermediario != null)
+                    {
+                        foreach (var result in intermediario.results)
+                        {
+                            var item = new SugestoesModel()
+                            {
+                                backdrop_path = result.backdrop_path,
+                                original_language = result.original_language,
+                                original_title = result.original_title,
+                                overview = result.overview,
+                                popularity = result.popularity,
+                                poster_path = result.poster_path,
+                                release_date = result.release_date,
+                                title = result.title,
+                                vote_average = result.vote_average,
+                                vote_count = result.vote_count
+                            };
+                            model.Add(item);
+                        }
+                    }
+                }                
+            }
+            return View(model);
         }
 
         [Authorize]
---
+++ filmes/Models/SugestoesModel.cs
@@ -0,0 +1,17 @@
+﻿namespace filmes.Models
+{
+    public class SugestoesModel
+    {
+        public string backdrop_path { get; set; } = string.Empty;
+        public string original_language { get; set; } = string.Empty;
+        public string original_title { get; set; } = string.Empty;
+        public string overview { get; set; } = string.Empty;
+        public decimal popularity { get; set; }
+        public string poster_path { get; set; } = string.Empty;
+        public DateTime release_date { get; set; }
+        public string title { get; set; } = string.Empty;
+        public decimal vote_average { get; set; }
+        public int vote_count { get; set; }
+
+    }
+}
